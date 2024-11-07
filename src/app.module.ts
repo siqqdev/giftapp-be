bb5e96c92370c370dee88b2afd502ae6efd915f9
siqq <144137886+siqqdev@users.noreply.github.com>
2024-11-07 22:39:22 +0100

    clean up app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { GiftModule } from './gift/gift.module';
 import { ActionModule } from './action/action.module';
-import { BotService } from './telegram/bot.service';
 import { APP_GUARD, Reflector } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
 import { BotModule } from './telegram/bot.module';
@@ -26,9 +23,7 @@ import { BuyGiftModule } from './buy/buyGift.module';
       BuyGiftModule,
       TransferGiftModule
   ],
-  controllers: [AppController],
   providers: [
-      AppService,
       {
           provide: APP_GUARD,
           useClass: AuthGuard,
